4cd631e195831425cf7b62fb43a76e3d6a14e08a
tri-white <olegovic101@gmail.com>
2023-10-06 16:23:30 +0300

    profile blade with user's info
    still doesn't show photo and shows only authorized user's profile, not everyone's

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,11 +5,11 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="profile-image mx-auto" style="height:150px; width:150px;">
-                <img src="{{ $user_data['photo'] ?? 'images/user_male.jpg' }}" style="width:100%; height:100%; object-fit: contain;"
+                <img src="{{ Auth::user()->photo ?? './images/user_male.jpg' }}" style="width:100%; height:100%; object-fit: contain;"
                      class="rounded-circle border border-1 border-dark" alt="Profile Picture">
             </div>
 
-            @if(isset($user_data['userid']) && $user_data['userid'] == request()->input('id'))
+            @if(Auth::check())
             <form action="{{ url('/change_profile_image') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="col-lg-12 my-2 align-items-center text-center">
@@ -21,17 +21,13 @@
             @endif
 
             <div class="profile-info text-center mt-2">
-                <h5>{{ $user_data['login'] }}</h5>
-                @if($admin == 1 && $user_data['userid'] != request()->input('id'))
+                <h5>{{ Auth::user()->login }}</h5>
+                @if(Auth::user()->admin == 1)
                     <a class="my-auto me-4 link-dark"
-                       href="{{ url('/remove_profile') }}/{{ $user_data['userid'] }}">
+                       href="">
                         <i class="fa fa-trash-can"></i>
                     </a>
                 @endif
-            </div>
-
-            <div class="profile-posts">
-                <h1>{{ $user_data['description'] }}</h1>
             </div>
         </div>
     </div>
@@ -44,21 +40,7 @@
                         Пости
                     </div>
                     <div class="col-lg-6 col-md-8 col-sm-12">
-                        @if(is_bool($posts))
-                            <div class='mb-5 text-muted col-lg-12 text-center display-4'>
-                                Не знайдено постів
-                            </div>
-                        @else
-                            @foreach($posts as $row)
-                                <?php
-                                $user_class = new User();
-                                $post_class = new Post();
-                                $row_user = $user_class->get_data($row['userid']);
-                                $row_post = $post_class->get_data($row['postid']);
-                                ?>
-                                @include('post-card_profile', ['row_user' => $row_user, 'row_post' => $row_post])
-                            @endforeach
-                        @endif
+                        <!-- here should be profile user's posts -->
                     </div>
                 </div>
             </div>
