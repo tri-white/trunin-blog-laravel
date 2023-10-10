07821e2cba532376aa9755376dc1bce271357e2b
tri-white <olegovic101@gmail.com>
2023-10-10 16:47:01 +0300

    styled post a little

--- resources/views/templates/like.blade.php
+++ resources/views/templates/like.blade.php
@@ -1,23 +1,23 @@
 <p class="my-auto me-2"> {{ $post->likes}} </p>
 
-                    @if(!Auth::check())
-                    <a href="{{ route('login') }}" class="btn btn-outline-danger" id="like-button">
-                        <i class="fa fa-heart"></i>
-                    </a>
-                    @else
-                        <form method="POST" action="{{ route('like', [$post->id, Auth::user()->id]) }}">
-                            @csrf
-                            @php 
-                                $like = App\Models\Like::where('userid', Auth::user()->id)->where('postid', $post->id)->first();
-                            @endphp
-                            @if(!$like)
-                                <button type="submit" class="btn btn-outline-danger" id="like-button">
-                                    <i class="fa fa-heart"></i>
-                                </button>
-                            @else
-                                <button type="submit" class="btn btn-danger" id="like-button">
-                                    <i class="fa fa-heart"></i>
-                                </button>
-                            @endif
-                        </form>
-                    @endif
+@if(!Auth::check())
+<a href="{{ route('login') }}" class="btn btn-outline-danger" id="like-button">
+    <i class="fa fa-heart"></i>
+</a>
+@else
+    <form method="POST" action="{{ route('like', [$post->id, Auth::user()->id]) }}">
+        @csrf
+        @php 
+            $like = App\Models\Like::where('userid', Auth::user()->id)->where('postid', $post->id)->first();
+        @endphp
+        @if(!$like)
+            <button type="submit" class="btn btn-outline-danger" id="like-button">
+                <i class="fa fa-heart"></i>
+            </button>
+        @else
+            <button type="submit" class="btn btn-danger" id="like-button">
+                <i class="fa fa-heart"></i>
+            </button>
+        @endif
+    </form>
+@endif
