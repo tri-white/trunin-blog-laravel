82196ac9369ad187355cc4500672fd77f0bffbeb
tri-white <olegovic101@gmail.com>
2023-12-04 20:36:17 +0200

    added migration for friends table

---
+++ database/migrations/2023_12_04_203537_create_friends_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('friend_id');
+            $table->timestamps();
+
+            // Add foreign keys
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('friend_id')->references('id')->on('users')->onDelete('cascade');
+
+            // Add unique constraint to prevent duplicate friendships
+            $table->unique(['user_id', 'friend_id']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
